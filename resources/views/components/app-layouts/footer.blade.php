6089e6bc3c58d5cfdfd20edc815ae0a023c9adb8
Johnathan Klimo <117110491+johnathan-klimo@users.noreply.github.com>
2025-11-18 16:39:40 +0000

    Initial WIP commit of initial WIP footer variant

--- resources/views/components/app-layouts/footer.blade.php
+++ resources/views/components/app-layouts/footer.blade.php
@@ -1,21 +1,39 @@
 <footer class="bg-sky-800 mt-8">
-  
-  <div class="flex justify-between py-4 max-w-6xl mx-auto">
-    
-    <div>
-      <h4 class="font-bold text-gray-50 pb-2">About This Site</h1>
-      <p class="text-gray-50">This is going to be the absolutely best grocery app ever!</p>
-    </div>
 
-    <div class="flex flex-col gap-x-6">
-      <h4 class="font-bold text-gray-50 pb-2">Site Navigation</h1>
-      <a href="/" wire:navigate class="text-gray-50 hover:text-orange-400">Home</a>
-      <a href="/recipes" wire:navigate class="text-gray-50 hover:text-orange-400">Recipes</a>
-      <a href="/grocery-items" wire:navigate class="text-gray-50 hover:text-orange-400">Grocery Items</a>
-      <a href="/grocery-categories" wire:navigate class="text-gray-50 hover:text-orange-400">Grocery Categories</a>
-      <a href="/brand-names" wire:navigate class="text-gray-50 hover:text-orange-400">Brand Names</a>
+  <div class="mx-auto max-w-6xl mt-8 px-4 2xl:px-0">
+    <div class="flex flex-col gap-x-8 lg:flex-row py-8">
+      <div class="lg:basis-1/2 sm:pr-20 md:pr-72 lg:pr-10">
+        <h4 class="font-bold text-gray-50">About This Site</h4>
+        <p class="text-gray-50 pt-4">
+          Insert initial portion of company blurb here...
+        </p>
+        <p class="text-gray-50 py-4">Insert second portion of company blurb here...</p>
+      </div>
+      <div class="lg:basis-1/4">
+        <h4 class="font-bold text-gray-50">Menu 1</h4>
+        <nav class="py-4">
+          <ul class="text-gray-50">
+            <li><a href="#" wire:navigate class="text-gray-50 hover:text-orange-400">Link 1</a></li>
+            <li><a href="#" wire:navigate class="text-gray-50 hover:text-orange-400">Link 2</a></li>
+            <li><a href="#" wire:navigate class="text-gray-50 hover:text-orange-400">Link 3</a></li>
+            <li><a href="#" wire:navigate class="text-gray-50 hover:text-orange-400">Link 4</a></li>
+            <li><a href="#" wire:navigate class="text-gray-50 hover:text-orange-400">Link 5</a></li>
+          </ul>
+        </nav>
+      </div>
+      <div class="lg:basis-1/4">
+        <h4 class="font-bold text-gray-50">Other Important Pages</h4>
+        <nav class="pt-4">
+          <ul class="text-gray-50">
+            <li><a href="#" wire:navigate class="text-gray-50 hover:text-orange-400">About Us</a></li>
+            <li><a href="#" wire:navigate class="text-gray-50 hover:text-orange-400">Privacy Policy</a></li>
+          </ul>
+        </nav>
+      </div>
     </div>
-  
+  </div>
+  <div class="mx-auto max-w-6xl py-4 px-4 2xl:px-0">
+    <p class="text-center text-gray-50">&copy; 2025 Acme Inc.</p>
   </div>
   
 </footer>
