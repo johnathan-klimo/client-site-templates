42e8158bbe3c6aa0fd3f2d3b004b32694f12b7eb
Johnathan Klimo <117110491+johnathan-klimo@users.noreply.github.com>
2025-11-19 16:41:11 +0000

    Minor revisions to initial testimonials block design

--- resources/views/components/app-testimonials/testimonial-block-1.blade.php
+++ resources/views/components/app-testimonials/testimonial-block-1.blade.php
@@ -1,32 +1,39 @@
-<div class="grid grid-cols-3 gap-6">
-  <x-app-testimonials.testimonial-card-1
-    fileName="professional-headshot-1"
-    text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit a deleniti minus dolorum. Temporibus iste magnam, cumque fugiat pariatur assumenda!"
-    clientName="Judy P."
-  />
-  <x-app-testimonials.testimonial-card-1
-    fileName="professional-headshot-2"
-    text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit vitae aliquam expedita? Ullam fugit repellendus sit error consequuntur quas fuga!"
-    clientName="Harry B."
-  />
-  <x-app-testimonials.testimonial-card-1
-    fileName="professional-headshot-3"
-    text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae repellendus praesentium dicta hic nobis dolorum blanditiis earum recusandae officiis eius?"
-    clientName="Theresa M."
-  />
-  <x-app-testimonials.testimonial-card-1
-    fileName="professional-headshot-5"
-    text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate harum illum cum tempora doloremque, sint maxime voluptates ipsa accusantium omnis!"
-    clientName="Matthew C."
-  />
-  <x-app-testimonials.testimonial-card-1
-    fileName="professional-headshot-4"
-    text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto quia sequi totam ipsa doloremque quas, tenetur ipsum natus ut numquam?"
-    clientName="Jennifer G."
-  />
-  <x-app-testimonials.testimonial-card-1
-    fileName="professional-headshot-6"
-    text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores assumenda in excepturi iste neque laudantium, dolor ducimus facilis libero ipsam."
-    clientName="Megan C."
-  />
+<div class="mt-6">
+  <div>
+    <h2 class="text-2xl text-center font-bold text-sky-800">Client Testimonials</h2>
+  </div>
+  <div class="mt-6">
+    <div class="grid grid-cols-3 gap-6">
+      <x-app-testimonials.testimonial-card-1
+        fileName="professional-headshot-1"
+        text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit a deleniti minus dolorum. Temporibus iste magnam, cumque fugiat pariatur assumenda!"
+        clientName="Judy P."
+      />
+      <x-app-testimonials.testimonial-card-1
+        fileName="professional-headshot-2"
+        text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit vitae aliquam expedita? Ullam fugit repellendus sit error consequuntur quas fuga!"
+        clientName="Harry B."
+      />
+      <x-app-testimonials.testimonial-card-1
+        fileName="professional-headshot-3"
+        text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae repellendus praesentium dicta hic nobis dolorum blanditiis earum recusandae officiis eius?"
+        clientName="Theresa M."
+      />
+      <x-app-testimonials.testimonial-card-1
+        fileName="professional-headshot-5"
+        text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate harum illum cum tempora doloremque, sint maxime voluptates ipsa accusantium omnis!"
+        clientName="Matthew C."
+      />
+      <x-app-testimonials.testimonial-card-1
+        fileName="professional-headshot-4"
+        text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto quia sequi totam ipsa doloremque quas, tenetur ipsum natus ut numquam?"
+        clientName="Jennifer G."
+      />
+      <x-app-testimonials.testimonial-card-1
+        fileName="professional-headshot-6"
+        text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores assumenda in excepturi iste neque laudantium, dolor ducimus facilis libero ipsam."
+        clientName="Megan C."
+      />
+    </div>
+  </div>
 </div>
