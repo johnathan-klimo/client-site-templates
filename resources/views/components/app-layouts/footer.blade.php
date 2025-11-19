df38e349840bf0d7b348001fcb585263c67670bf
Johnathan Klimo <117110491+johnathan-klimo@users.noreply.github.com>
2025-11-19 15:56:56 +0000

    Minor revisions to initial footer design

--- resources/views/components/app-layouts/footer.blade.php
+++ resources/views/components/app-layouts/footer.blade.php
@@ -1,41 +1,39 @@
 <footer class="bg-sky-800 mt-8">
 
   <div class="mx-auto max-w-6xl mt-8 px-4 2xl:px-0">
-    <div class="flex flex-col gap-x-8 lg:flex-row py-8">
+    <div class="flex flex-col gap-x-8 lg:flex-row pt-8">
       <div class="lg:basis-1/2 sm:pr-20 md:pr-72 lg:pr-10">
         <h4 class="font-bold text-gray-50">About This Site</h4>
-        <p class="text-gray-50 pt-4">
-          Insert initial portion of company blurb here...
+        <p class="text-gray-200 pt-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, maiores tenetur cumque quidem fugit repellat perferendis explicabo laboriosam, vitae, ex illo voluptas assumenda distinctio natus tempora magni iusto a perspiciatis.
         </p>
-        <p class="text-gray-50 py-4">Insert second portion of company blurb here...</p>
+        <p class="text-gray-200 py-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, dolorem.</p>
       </div>
       <div class="lg:basis-1/4">
         <h4 class="font-bold text-gray-50">Menu 1</h4>
         <nav class="py-4">
-          <ul class="text-gray-50">
-            <li><a href="#" wire:navigate class="text-gray-50 hover:text-orange-400">Link 1</a></li>
-            <li><a href="#" wire:navigate class="text-gray-50 hover:text-orange-400">Link 2</a></li>
-            <li><a href="#" wire:navigate class="text-gray-50 hover:text-orange-400">Link 3</a></li>
-            <li><a href="#" wire:navigate class="text-gray-50 hover:text-orange-400">Link 4</a></li>
-            <li><a href="#" wire:navigate class="text-gray-50 hover:text-orange-400">Link 5</a></li>
+          <ul class="text-gray-200">
+            <li><a href="#" wire:navigate class="hover:text-orange-400">Internal Site Link 1</a></li>
+            <li><a href="#" wire:navigate class="hover:text-orange-400">Internal Site Link 2</a></li>
+            <li><a href="#" wire:navigate class="hover:text-orange-400">Internal Site Link 3</a></li>
+            <li><a href="#" wire:navigate class="hover:text-orange-400">Internal Site Link 4</a></li>
+            <li><a href="#" wire:navigate class="hover:text-orange-400">Internal Site Link 5</a></li>
           </ul>
         </nav>
       </div>
       <div class="lg:basis-1/4">
-        <h4 class="font-bold text-gray-50">Other Important Pages</h4>
+        <h4 class="font-bold text-gray-50">Menu 2</h4>
         <nav class="pt-4">
-          <ul class="text-gray-50">
-            <li><a href="#" wire:navigate class="text-gray-50 hover:text-orange-400">About Us</a></li>
-            <li><a href="#" wire:navigate class="text-gray-50 hover:text-orange-400">Privacy Policy</a></li>
+          <ul class="text-gray-200">
+            <li><a href="#" wire:navigate class="hover:text-orange-400">Internal Site Link 6</a></li>
+            <li><a href="#" wire:navigate class="hover:text-orange-400">Internal Site Link 7</a></li>
           </ul>
         </nav>
       </div>
     </div>
   </div>
   <div class="mx-auto max-w-6xl py-4 px-4 2xl:px-0">
-    <p class="text-center text-gray-50">&copy; 2025 Acme Inc.</p>
+    <p class="text-center text-gray-200">&copy; 2025 Acme Inc.</p>
   </div>
   
 </footer>
-
-
