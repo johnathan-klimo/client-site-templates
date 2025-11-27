7d7e1114170a3bdb4b27acc4a640e769affdd908
Johnathan Klimo <117110491+johnathan-klimo@users.noreply.github.com>
2025-11-27 17:29:12 +0000

    Second commit for fourth batch of Service blocks and cards

--- resources/views/components/app-services/service-card-4.blade.php
+++ resources/views/components/app-services/service-card-4.blade.php
@@ -5,13 +5,13 @@
   $iconComponentSize = 'size-' . $serviceIconSize;
 @endphp
 
-<div class="grid grid-cols-12">
-  <div class="col-start-2 col-end-7 overflow-hidden bg-cover rounded-lg shadow-xl">
+<div class="px-6 grid grid-cols-2 gap-10">
+  <div class="overflow-hidden bg-cover rounded-lg shadow-xl">
     <a href="#">
       <img class="transform transition-transform duration-300 hover:scale-110 hover:shadow-2xl will-change-transform" src="{{ asset('images/' . $fileName . '.jpg') }}" alt="">  
     </a>
   </div>
-  <div class="col-start-8 col-end-12 bg-neutral-50 border border-gray-100 rounded-lg shadow-xl text-gray-900 flex flex-col items-center justify-center">
+  <div class="m-4 px-8 pt-6 bg-neutral-50 border border-gray-100 rounded-lg shadow-xl text-gray-900 flex flex-col items-center">
     <div>
       <x-dynamic-component
         :component="$iconComponentName"
@@ -21,10 +21,10 @@
     <div class="mt-6">
       <h3 class="text-lg font-bold">{{ $serviceName }}</h3>
     </div>
-    <div>
-      <p class="p-4">{{ $serviceDescription }}</p>
+    <div class="mt-4">
+      <p>{{ $serviceDescription }}</p>
     </div>
-    <div class="mt-6 pb-4">
+    <div class="pt-8 pb-4">
       <a href="#" class="bg-sky-800 hover:bg-orange-400 text-gray-50 hover:text-gray-900 py-3 px-6 rounded-full shadow-md">
         Learn More
       </a>
