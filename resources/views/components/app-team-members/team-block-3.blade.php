d9527ac34ebdd2e27fabb3272dfeea3b83990ab7
Johnathan Klimo <117110491+johnathan-klimo@users.noreply.github.com>
2025-11-28 22:37:38 +0000

    Second commit of third version of Team block and cards

--- resources/views/components/app-team-members/team-block-3.blade.php
+++ resources/views/components/app-team-members/team-block-3.blade.php
@@ -3,69 +3,54 @@
     <h2 class="text-2xl text-center font-bold text-sky-800">Our Team [Option 3]</h2>
   </div>
   <div class="mt-8">
-    <div>
+    <div class="grid grid-cols-2 gap-x-10 gap-y-14">
       <x-app-team-members.team-card-3
         fileName="professional-headshot-6-large"
         teamMemberName="Gabriela C."
         teamMemberTitle="Co-Founder & CEO"
-        teamMemberBio="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis perspiciatis voluptas deleniti neque eum dolorem explicabo maiores harum tempore. Dicta!"
+        teamMemberBio="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis perspiciatis voluptas deleniti neque eum dolorem explicabo maiores harum tempore."
       />
-    </div>
-    <div class="mt-8">
       <x-app-team-members.team-card-3
         fileName="professional-headshot-5-large"
         teamMemberName="Jason D."
         teamMemberTitle="Co-Founder & CFO"
         teamMemberBio="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis perspiciatis voluptas deleniti neque eum dolorem explicabo maiores harum tempore. Dicta!"
       />
-    </div>
-    <div class="mt-8">
       <x-app-team-members.team-card-3
         fileName="professional-headshot-4-large"
         teamMemberName="Theresa M."
         teamMemberTitle="Chief Technology Officer"
         teamMemberBio="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis perspiciatis voluptas deleniti neque eum dolorem explicabo maiores harum tempore. Dicta!"
       />
-    </div>
-    <div>
-      <<x-app-team-members.team-card-3
+      <x-app-team-members.team-card-3
         fileName="professional-headshot-3-large"
         teamMemberName="Jessica Z."
         teamMemberTitle="Manager, Web Development"
         teamMemberBio="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis perspiciatis voluptas deleniti neque eum dolorem explicabo maiores harum tempore. Dicta!"
       />
-    </div>
-    <div class="mt-8">
       <x-app-team-members.team-card-3
         fileName="professional-headshot-2-large"
         teamMemberName="Mark M."
         teamMemberTitle="Manager, Marketing"
         teamMemberBio="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis perspiciatis voluptas deleniti neque eum dolorem explicabo maiores harum tempore. Dicta!"
       />
-    </div>
-    <div class="mt-8">
       <x-app-team-members.team-card-3
         fileName="professional-headshot-1-large"
         teamMemberName="Amanada S."
         teamMemberTitle="Manager, Business Development"
         teamMemberBio="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis perspiciatis voluptas deleniti neque eum dolorem explicabo maiores harum tempore. Dicta!"
       />
-    </div>
-    <div class="mt-8">
       <x-app-team-members.team-card-3
         fileName="professional-headshot-7-large"
         teamMemberName="Asha B."
         teamMemberTitle="Full-Stack Developer"
         teamMemberBio="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis perspiciatis voluptas deleniti neque eum dolorem explicabo maiores harum tempore. Dicta!"
       />
-    </div>
-    <div class="mt-8">
       <x-app-team-members.team-card-3
         fileName="professional-headshot-8-large"
         teamMemberName="Sarah R."
         teamMemberTitle="Full-Stack Developer"
         teamMemberBio="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis perspiciatis voluptas deleniti neque eum dolorem explicabo maiores harum tempore. Dicta!"
       />
-    </div>
   </div>
 </div>
--- resources/views/components/app-team-members/team-card-3.blade.php
+++ resources/views/components/app-team-members/team-card-3.blade.php
@@ -1,17 +1,17 @@
 @props(['fileName' => '', 'teamMemberName' => '', 'teamMemberTitle' => '', 'teamMemberBio' => ''])
 
-<div class="px-6 grid grid-cols-2 gap-10">
-  <div class="p-4 flex items-center justify-center">
-    <img class="size-56 rounded-lg shadow-md" src="{{ asset('images/' . $fileName . '.jpg') }}" alt="">  
+<div class="grid grid-cols-3 gap-4">
+  <div class="flex items-center justify-center">
+    <img class="size-48 rounded-lg shadow-md" src="{{ asset('images/' . $fileName . '.jpg') }}" alt="">  
   </div>
-  <div class="m-4 px-8 pt-6 bg-neutral-50 border border-gray-100 rounded-lg shadow-xl text-gray-900 flex flex-col items-center">
-    <div class="px-4 font-semibold text-center">
+  <div class="py-4 col-span-2 bg-neutral-50 border border-gray-100 rounded-lg shadow-lg text-gray-900 flex flex-col">
+    <div class="px-4 font-semibold">
       <h3>{{ $teamMemberName }}</h3>
     </div>
-    <div class="px-4 text-sm text-center">
+    <div class="px-4 text-sm">
       <h4>{{ $teamMemberTitle }}</h4>
     </div>
-    <div class="mt-4">
+    <div class="pt-4 px-4">
       <p>{{ $teamMemberBio }}</p>
     </div>
   </div>
